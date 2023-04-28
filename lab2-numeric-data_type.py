0b97360cb1f26831c543b7cfe5f21eff39d0ca47
Pooja <heerapooja13@gmail.com>
2023-04-28 22:25:51 +0530

    lab 1, lab2, lab3 done

---
+++ lab2-numeric-data_type.py
@@ -0,0 +1,29 @@
+#Lab 1 Hello world program
+print("hello world!")
+print("Python has three numeric types: int, float, and complex")
+
+#Lab 2 Working with Numeric Data Types
+
+#Exercise 2: Introducing the int data type
+val=1
+print(val)
+print(type(val)) 
+print(str(val)+" is of the data type " + str(type(val)))
+
+#Exercise 3: Introducing the float data type
+fval=3.14
+print(fval)
+print(type(fval))
+print(str(fval)+" is of the data type " + str(type(fval)))
+
+#Exercise 4: Introducing the complex data type
+val1=5j
+print(val1)
+print(type(val1))
+print(str(val1)+" is of data type "+str(type(val1)))
+
+#Exercise 5: Introducing the bool data type
+bval=True
+print(bval)
+print(type(bval))
+print(str(bval)+ " is of data type "+str(type(bval)))
